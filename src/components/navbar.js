1a140efd7ccaf4a9bc6c82d29b6164fe7687bb89
Tom Simmons <tomsimmons@Toms-MacBook-Pro.local>
2019-08-07 11:51:17 +0100

    add nav styling

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,11 +1,16 @@
 import React from "react"
 import { Link } from "gatsby"
+import styles from "./navbar.module.css"
 
 export default function Navbar() {
   return (
-    <div>
-      <Link to="/">Home</Link>
-      <Link to="/blog">Blog</Link>
-    </div>
+    <nav className={styles.navbar}>
+      <Link className={styles.navbarLink} to="/">
+        Home
+      </Link>
+      <Link className={styles.navbarLink} to="/blog">
+        Blog
+      </Link>
+    </nav>
   )
 }
