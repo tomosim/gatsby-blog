f3872f15827f95d3325c4c1b7488211e297c1815
Tom Simmons <tomsimmons@Toms-MacBook-Pro.local>
2019-08-09 12:06:37 +0100

    format header

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,10 @@
 import PropTypes from "prop-types"
 import React from "react"
+import styles from "./header.module.css"
 
 const Header = ({ siteTitle }) => (
   <header>
-    <h1 style={{ fontSize: "50pt" }}>{siteTitle}</h1>
+    <h1 className={styles.title}>{siteTitle}</h1>
   </header>
 )
 
