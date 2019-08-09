54b1246ae568ef2681321877b329702d29d6b40b
Tom Simmons <tomsimmons@Toms-MacBook-Pro.local>
2019-08-09 09:42:45 +0100

    add BlogCard to BlogList

--- src/components/blog-list.js
+++ src/components/blog-list.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react"
-// import BlogCard from "../components/blog-card"
+import BlogCard from "../components/blog-card"
 import styles from "./blog-list.module.css"
 
 export default class BlogList extends Component {
@@ -7,8 +7,8 @@ export default class BlogList extends Component {
     return (
       <div className={styles.blogGrid}>
         {this.props.blogs.map(blog => {
-          return <h3>{blog.node.frontmatter.title}</h3>
-          //   return <BlogCard key={blog.node.frontmatter.title} blog={blog} />
+          //   return <h3>{blog.node.frontmatter.title}</h3>
+          return <BlogCard key={blog.node.frontmatter.title} blog={blog} />
         })}
       </div>
     )
