531b86dec28b3475b000502bf776e58912395248
Tom Simmons <tomsimmons@Toms-MacBook-Pro.local>
2019-08-09 17:04:07 +0100

    fix up blogpost page

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,21 +1,45 @@
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { graphql } from "gatsby"
+import Header from "../components/header"
+import Img from "gatsby-image"
+import styles from "./blog-post.module.css"
 
-export default function BlogPost({ pageContext }) {
-  const { slug } = pageContext
+export default function BlogPost({ pageContext, data }) {
+  // const { slug } = pageContext
+  const post = data.markdownRemark
+  const featuredImgFluid = post.frontmatter.primaryImage.childImageSharp.fluid
   return (
     <Layout>
       <SEO title="Blog" />
-      <div>
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <h1>{slug}</h1>
-        <p>Lorem blah blah blipsom</p>
-      </div>
+      <main className={styles.blogMain}>
+        <h1>{data.markdownRemark.frontmatter.title}</h1>
+        <Img fluid={featuredImgFluid}></Img>
+        <div
+          dangerouslySetInnerHTML={{ __html: data.markdownRemark.excerpt }}
+        ></div>
+      </main>
     </Layout>
   )
 }
+
+export const blogPageQuery = graphql`
+  query PostBySlug($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      excerpt(format: HTML, pruneLength: 10000)
+      html
+      frontmatter {
+        title
+        date(formatString: "MMMM DD, YYYY")
+        primaryImage {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`
