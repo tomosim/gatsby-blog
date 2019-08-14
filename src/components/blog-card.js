1e1f095986a61f4566734e2f0d47f56f0675fdfd
Tom Simmons <tomsimmons@Toms-MacBook-Pro.local>
2019-08-14 14:12:44 +0100

    blog card redesign

--- src/components/blog-card.js
+++ src/components/blog-card.js
@@ -6,12 +6,12 @@ const BlogCard = ({ blog: { node } }) => {
   const path = `/blog/${node.fields.slug}`
   return (
     <div className={styles.blogCard}>
-      <h4 className={styles.date}>{node.frontmatter.date}</h4>
-      <Link to={path}>
-        <header className={styles.title}>
-          <h2>{node.frontmatter.title}</h2>
-        </header>
-      </Link>
+      <header className={styles.header}>
+        <Link to={path}>
+          <h2 className={styles.title}>{node.frontmatter.title}</h2>
+        </Link>
+        <h4 className={styles.date}>{node.frontmatter.date}</h4>
+      </header>
       <article className={styles.description}>
         <p>{node.excerpt}</p>
       </article>
