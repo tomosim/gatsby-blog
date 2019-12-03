caccb91d49b2722943ef0361b5ccc226519cf130
Tom Simmons <tomsimmons@Toms-MacBook-Pro-2.local>
2019-12-03 10:25:13 +0000

    edit about section

--- src/components/about.js
+++ src/components/about.js
@@ -3,8 +3,12 @@ import styles from "./about.module.css"
 export default function About() {
   return (
     <section className={styles.about}>
-      <h2> ⛰</h2>
-      <p>Welcome to my site. This is the about section. I love the peaks</p>
+      <h2>⛰</h2>
+      <p>
+        Ex-Army Officer trying to find excuses to stay outside and seek
+        adventure.  Currently working in Outdoor Leadership and climbing and
+        hiking around New Zealand.
+      </p>
       <hr />
     </section>
   )
