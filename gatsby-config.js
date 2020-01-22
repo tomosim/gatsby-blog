211915aed062028a5ae7d7a6509f9639d8601fff
Tom Simmons <tomsimmons@Toms-MacBook-Pro-2.local>
2020-01-22 14:50:50 +0000

    set up netlify CMS

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,7 @@ module.exports = {
     author: `@tomosim`,
   },
   plugins: [
+    `gatsby-plugin-netlify-cms`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
@@ -32,7 +33,6 @@ module.exports = {
       resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [
-
           {
             resolve: `gatsby-remark-images`,
             options: {
