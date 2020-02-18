439471a1b5b0ae678e3efea1b4d94f1566d83b96
Tom Simmons <tomsimmons@Toms-MacBook-Pro-2.local>
2020-02-18 09:21:18 +0000

    more compact mobile design

--- src/components/about.js
+++ src/components/about.js
@@ -3,13 +3,12 @@ import styles from "./about.module.css"
 export default function About() {
   return (
     <section className={styles.about}>
-      <h2>⛰</h2>
+      <h2 className={styles.emoji}>⛰</h2>
       <p>
         Ex-Army Officer trying to find excuses to stay outside and seek
         adventure.  Currently working in Outdoor Leadership and climbing and
         hiking around New Zealand.
       </p>
-      <hr />
     </section>
   )
 }
